815b5a4e95ef9977630ef6c07b9fbe0cd43e9ace
Corey Williams <corey.w108@gmail.com>
2026-06-20 10:22:28 -0500

    NUMBER of debug fixes, introduced DeviceAddress and TextureIndex type, mostly for pretty printing. added acquire type to acquirements, so that present acquires can be treated a bit differently. improved shader reflection. the current goal is to have asserts for buffer and shader alignment

--- LAB/Vector/VectorTemplate.h
+++ LAB/Vector/VectorTemplate.h
@@ -16,6 +16,6 @@ namespace lab {
 	//F short for floating point, can be double or float, Dimensions is short for dimensions
 	template<std::floating_point F, uint8_t Dimensions> 
 	requires((Dimensions > 1) && (Dimensions <= 4))
-	struct Vector{};
+	struct Vector;
 }
 
